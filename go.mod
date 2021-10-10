555ca94bf88cecaa1864d21ea73542f5da45121a
rcarrata <rcarratalasanchez@gmail.com>
2021-10-10 15:45:42 +0200

    added structure of db working

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module jwt-practice
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.1.1
+)
+
+require github.com/jinzhu/inflection v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/lib/pq"
 )
 
 var router *mux.Router
@@ -13,7 +17,7 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name"`
 	Email    string `gorm:"unique" json:"email"`
-	Password string `json:"password`
+	Password string `json:"password"`
 	Role     string `json:"role"`
 }
 
@@ -23,7 +27,7 @@ type Authentication struct {
 	Password string `json:"password"`
 }
 
-// Token 
+// Token
 type Token struct {
 	Role        string `json:"role"`
 	Email       string `json:"email"`
@@ -41,8 +45,69 @@ func InitializeRoute() {
 	router.HandleFunc("/signin", SignIn).Methods("POST")
 }
 
+func SignUp(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func SignIn(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Connect to the Postgresql Database
+// TODO: Use Viper and .Env to handle the psql parameters
+func GetDatabase() *gorm.DB {
+	databasename := "userdb"
+	database := "postgres"
+	databasepassword := "1312"
+	databaseurl := "postgres://postgres:" + databasepassword + "@localhost/" + databasename + "?sslmode=disable"
+
+	connection, err := gorm.Open(database, databaseurl)
+	if err != nil {
+		log.Fatalln("wrong database url")
+	}
+	if err != nil {
+		log.Fatalf("Error in connect the DB %v", err)
+		return nil
+	}
+	if err := connection.DB().Ping(); err != nil {
+		log.Fatalln("Error in make ping the DB " + err.Error())
+		return nil
+	}
+	if connection.Error != nil {
+		log.Fatalln("Any Error in connect the DB " + err.Error())
+		return nil
+	}
+	log.Println("DB connected")
+	return connection
+
+}
+
+func InitialMigration() {
+	connection := GetDatabase()
+	defer Closedatabase(connection)
+	connection.AutoMigrate(User{})
+	CreateRecord(connection)
+}
+
+func Closedatabase(connection *gorm.DB) {
+	log.Println("Closing DB connection")
+	sqldb := connection.DB()
+	sqldb.Close()
+}
+
+// Function to test the generation of records in the DB
+func CreateRecord(db *gorm.DB) {
+	user := User{Name: "Rober", Email: "rober@test.com", Password: "test", Role: "Admin"}
+	result := db.Create(&user)
+
+	if result.Error != nil {
+		log.Fatalln("Not able to generate the record")
+	}
+}
+
 // Main entry point function
 func main() {
 	CreateRouter()
 	InitializeRoute()
+	InitialMigration()
 }
